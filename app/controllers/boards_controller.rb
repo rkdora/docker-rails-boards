bc9ad122d740b44ba2f7670467c32eabb56bf90d
ryuto <rryuto5210@gmail.com>
2021-03-14 23:29:00 +0900

    refactor: board update

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -29,9 +29,14 @@ class BoardsController < ApplicationController
   def edit; end
 
   def update
-    @board.update(board_params)
-
-    redirect_to @board
+    if @board.update(board_params)
+      redirect_to @board, flash: {notice: "「#{@board.title}」の掲示板が更新されました"}
+    else
+      redirect_to :back, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
   end
 
   def destroy
